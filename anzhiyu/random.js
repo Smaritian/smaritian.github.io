a40cb1a256df7bc42555fff79e9a7c594895f895
Smaritian <czx1370357098@gmail.com>
2025-03-19 16:28:53 +0800

    Site updated: 2025-03-19 16:28:52

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/18/hello-world/","2025/03/18/Hello-Hexo/"];function toRandomPost(){
+var posts=["2025/03/18/Hello-Hexo/","2025/03/18/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
